aee532063a48f9ccab25a0a47d63cd2fef962ede
Joshua Ather <joshuaather5@gmail.com>
2021-01-26 01:56:47 +0700

    Membuat Routes, Controller, View - Home dan About

---
+++ app/Views/Pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro tempore similique ipsam eos voluptatibus? Voluptate, ducimus suscipit aspernatur quam omnis sint ratione sunt eligendi alias, nulla earum, laborum illum amet.</p>
+        </div>
+    </div>
+</div>
---
+++ app/Views/Pages/home.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Home</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas corporis culpa distinctio veritatis fugit natus, illum reiciendis molestias assumenda suscipit quaerat pariatur, maxime adipisci quod, beatae libero cumque! Sint, cum!</p>
+
+        </div>
+    </div>
+</div>
